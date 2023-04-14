01b6fc9023d3d7b65694e2471546bc4178a0b87c
Kokcr3ator <117846571+Kokcr3ator@users.noreply.github.com>
2023-04-14 16:10:48 +0200

    Updated prova.py

--- prova.py
+++ prova.py
@@ -5,6 +5,7 @@ import pandas as pd
 import tensorflow as tf
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 mpl.rcParams['figure.figsize'] = (8, 6)
@@ -36,7 +37,7 @@ df['Day cos'] = np.cos(timestamp_s * (2 * np.pi / day))
 df['Year sin'] = np.sin(timestamp_s * (2 * np.pi / year))
 df['Year cos'] = np.cos(timestamp_s * (2 * np.pi / year))
 
-# Checking that the periodicity is daily and yearly
+## Checking that the periodicity is daily and yearly
 
 # fft = tf.signal.rfft(df['dam'])
 # f_per_dataset = np.arange(0, len(fft))
@@ -63,6 +64,7 @@ test_df = df[int(n*0.9):]
 
 num_features = df.shape[1]
 
+# Standardizing the features using only the training set
 
 train_mean = train_df.mean()
 train_std = train_df.std()
@@ -71,8 +73,241 @@ train_df = (train_df - train_mean) / train_std
 val_df = (val_df - train_mean) / train_std
 test_df = (test_df - train_mean) / train_std
 
-df_std = (df - train_mean) / train_std
-df_std = df_std.melt(var_name='Column', value_name='Normalized')
-plt.figure(figsize=(12, 6))
-ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
-_ = ax.set_xticklabels(df.keys(), rotation=90)
+## Plotting the standardized variables, in this case we can observe that some features such as gas price, thermal and the imports have
+## heavy tails  (some outliers are observed)
+
+# df_std = (df - train_mean) / train_std
+# df_std = df_std.melt(var_name='Column', value_name='Normalized')
+# plt.figure(figsize=(12, 6))
+# ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
+# _ = ax.set_xticklabels(df.keys(), rotation=90)
+# plt.show()
+
+## Defining how to create windows of datas
+
+class WindowGenerator():
+  def __init__(self, input_width, label_width, shift,
+               train_df=train_df, val_df=val_df, test_df=test_df,
+               label_columns=None):
+    # Store the raw data.
+    self.train_df = train_df
+    self.val_df = val_df
+    self.test_df = test_df
+
+    # Work out the label column indices.
+    self.label_columns = label_columns
+    if label_columns is not None:
+      self.label_columns_indices = {name: i for i, name in
+                                    enumerate(label_columns)}
+    self.column_indices = {name: i for i, name in
+                           enumerate(train_df.columns)}
+
+    # Work out the window parameters.
+    self.input_width = input_width
+    self.label_width = label_width
+    self.shift = shift
+
+    self.total_window_size = input_width + shift
+
+    self.input_slice = slice(0, input_width)
+    self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+    self.label_start = self.total_window_size - self.label_width
+    self.labels_slice = slice(self.label_start, None)
+    self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+  def __repr__(self):
+    return '\n'.join([
+        f'Total window size: {self.total_window_size}',
+        f'Input indices: {self.input_indices}',
+        f'Label indices: {self.label_indices}',
+        f'Label column name(s): {self.label_columns}'])
+  
+  
+## split_window allows to stack different windows
+
+def split_window(self, features):
+  inputs = features[:, self.input_slice, :]
+  labels = features[:, self.labels_slice, :]
+  if self.label_columns is not None:
+    labels = tf.stack(
+        [labels[:, :, self.column_indices[name]] for name in self.label_columns],
+        axis=-1)
+
+  # Slicing doesn't preserve static shape information, so set the shapes
+  # manually. This way the `tf.data.Datasets` are easier to inspect.
+  inputs.set_shape([None, self.input_width, None])
+  labels.set_shape([None, self.label_width, None])
+
+  return inputs, labels
+
+WindowGenerator.split_window = split_window
+
+## Defining a plot method to visualize the windows
+
+def plot(self, model=None, plot_col='dam', max_subplots=3):
+  inputs, labels = self.example
+  plt.figure(figsize=(12, 8))
+  plot_col_index = self.column_indices[plot_col]
+  max_n = min(max_subplots, len(inputs))
+  for n in range(max_n):
+    plt.subplot(max_n, 1, n+1)
+    plt.ylabel(f'{plot_col} [normed]')
+    plt.plot(self.input_indices, inputs[n, :, plot_col_index],
+             label='Inputs', marker='.', zorder=-10)
+
+    if self.label_columns:
+      label_col_index = self.label_columns_indices.get(plot_col, None)
+    else:
+      label_col_index = plot_col_index
+
+    if label_col_index is None:
+      continue
+
+    plt.scatter(self.label_indices, labels[n, :, label_col_index],
+                edgecolors='k', label='Labels', c='#2ca02c', s=64)
+    if model is not None:
+      predictions = model(inputs)
+      plt.scatter(self.label_indices, predictions[n, :, label_col_index],
+                  marker='X', edgecolors='k', label='Predictions',
+                  c='#ff7f0e', s=64)
+
+    if n == 0:
+      plt.legend()
+
+  plt.xlabel('Time [h]')
+
+WindowGenerator.plot = plot
+
+## Trying out the methods just implemented
+
+# w1 = WindowGenerator(input_width=24, label_width=1, shift=1, label_columns=['dam'])
+# example_window = tf.stack([np.array(train_df[:w1.total_window_size]),
+#                            np.array(train_df[100:100+w1.total_window_size]),
+#                            np.array(train_df[200:200+w1.total_window_size])])
+
+# example_inputs, example_labels = w1.split_window(example_window)
+# w1.example = example_inputs, example_labels
+# w1.plot()
+# plt.show()
+
+## This make_dataset method will take a time series DataFrame and convert it to a tf.data.Dataset
+##  of (input_window, label_window) pairs using the tf.keras.utils.timeseries_dataset_from_array function
+
+
+def make_dataset(self, data):
+  data = np.array(data, dtype=np.float32)
+  ds = tf.keras.utils.timeseries_dataset_from_array(
+      data=data,
+      targets=None,
+      sequence_length=self.total_window_size,
+      sequence_stride=1,
+      shuffle=True,
+      batch_size=32,)
+
+  ds = ds.map(self.split_window)
+
+  return ds
+
+WindowGenerator.make_dataset = make_dataset
+
+@property
+def train(self):
+  return self.make_dataset(self.train_df)
+
+@property
+def val(self):
+  return self.make_dataset(self.val_df)
+
+@property
+def test(self):
+  return self.make_dataset(self.test_df)
+
+@property
+def example(self):
+  """Get and cache an example batch of `inputs, labels` for plotting."""
+  result = getattr(self, '_example', None)
+  if result is None:
+    # No example batch was found, so get one from the `.train` dataset
+    result = next(iter(self.train))
+    # And cache it for next time
+    self._example = result
+  return result
+
+WindowGenerator.train = train
+WindowGenerator.val = val
+WindowGenerator.test = test
+WindowGenerator.example = example
+
+################# SINGLE STEP METHOD #################
+
+# Creating the Baseline (this predictor gives in outp put just the energy price in the previous observation)
+
+single_step_window = WindowGenerator(
+    input_width=1, label_width=1, shift=1,
+    label_columns=['dam'])
+
+class Baseline(tf.keras.Model):
+  def __init__(self, label_index=None):
+    super().__init__()
+    self.label_index = label_index
+
+  def call(self, inputs):
+    if self.label_index is None:
+      return inputs
+    result = inputs[:, :, self.label_index]
+    return result[:, :, tf.newaxis]
+  
+baseline = Baseline(label_index=column_indices['dam'])
+
+baseline.compile(loss=tf.keras.losses.MeanSquaredError(),
+                 metrics=[tf.keras.metrics.MeanAbsoluteError()])
+
+val_performance = {}
+performance = {}
+val_performance['Baseline'] = baseline.evaluate(single_step_window.val)
+performance['Baseline'] = baseline.evaluate(single_step_window.test, verbose=0)
+
+
+wide_window = WindowGenerator(
+    input_width=24, label_width=24, shift=1,
+    label_columns=['dam'])
+
+print('Input shape:', wide_window.example[0].shape)
+print('Output shape:', baseline(wide_window.example[0]).shape)
+
+wide_window.plot(baseline)
+plt.show()
+
+# Building the linear model
+
+linear = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=1)])
+
+MAX_EPOCHS = 20
+
+def compile_and_fit(model, window, patience=2):
+  early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                    patience=patience,
+                                                    mode='min')
+
+  model.compile(loss=tf.keras.losses.MeanSquaredError(),
+                optimizer=tf.keras.optimizers.Adam(),
+                metrics=[tf.keras.metrics.MeanAbsoluteError()])
+
+  history = model.fit(window.train, epochs=MAX_EPOCHS,
+                      validation_data=window.val,
+                      callbacks=[early_stopping])
+  return history
+
+history = compile_and_fit(linear, single_step_window)
+
+val_performance['Linear'] = linear.evaluate(single_step_window.val)
+performance['Linear'] = linear.evaluate(single_step_window.test, verbose=0)
+
+wide_window.plot(linear)
+plt.show()
+
+
+
+
